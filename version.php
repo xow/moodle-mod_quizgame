0cb7a07bc6b056ba546829d0d0e9b714ca1d1c92
John Okely <john@moodle.com>
2014-09-08 13:34:40 +0800

    Fix version number format and revision in version.php

--- version.php
+++ version.php
@@ -29,9 +29,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2014051201.01;               // If version == 0 then module will not be installed
+$plugin->version   = 2014051201;               // If version == 0 then module will not be installed
 
 $plugin->requires  = 2014051200.00;      // Requires this Moodle version (2.7)
 $plugin->cron      = 0;               // Period for cron to check this module (secs)
 $plugin->component = 'mod_quizgame'; // To check on upgrade, that module sits in correct place
 $plugin->maturity = MATURITY_ALPHA;
+$plugin->release = 'v2.7-r1';
