6c8fb3017390ee91127a5a733b3ebb2b2180524e
John Okely <john@moodle.com>
2015-03-12 15:59:49 +0800

    Release v2.8-r4

--- version.php
+++ version.php
@@ -29,10 +29,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015011300;               // If version == 0 then module will not be installed
+$plugin->version   = 2015031200;               // If version == 0 then module will not be installed
 
 $plugin->requires  = 2014051200.00;      // Requires this Moodle version (2.7)
 $plugin->cron      = 0;               // Period for cron to check this module (secs)
 $plugin->component = 'mod_quizgame'; // To check on upgrade, that module sits in correct place
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->release = 'v2.8-r3';
+$plugin->release = 'v2.8-r4';
