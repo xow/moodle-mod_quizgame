c558a124af99e29fe7de93f0e534b93606a40a16
John Okely <john@moodle.com>
2015-12-09 14:37:44 +0800

    Release 3.0-r3

--- version.php
+++ version.php
@@ -28,10 +28,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015120800;     // If version == 0 then module will not be installed.
+$plugin->version   = 2015120900;     // If version == 0 then module will not be installed.
 
 $plugin->requires  = 2014051200.00;  // Requires this Moodle version (2.7)
 $plugin->cron      = 0;              // Period for cron to check this module (secs).
 $plugin->component = 'mod_quizgame'; // To check on upgrade, that module sits in correct place.
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->release = 'v3.0-r2';
+$plugin->release = 'v3.0-r3';
