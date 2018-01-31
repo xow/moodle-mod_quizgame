31d286d5f1303179b0ae094cf06153cefa119fd8
Stephen Bourget <steve.bourget@sau19.org>
2018-01-30 20:26:12 -0500

    mod_quizgame: Add Backup / Restore support
    Fix issue #31

---
+++ backup/moodle2/backup_quizgame_stepslib.php
@@ -0,0 +1,76 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package mod_quizgame
+ * @subpackage backup-moodle2
+ * @copyright 2018 Stephen Bourget
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Define the complete quizgame structure for backup, with file and id annotations
+ */
+class backup_quizgame_activity_structure_step extends backup_activity_structure_step {
+
+    protected function define_structure() {
+
+        // To know if we are including userinfo.
+        $userinfo = $this->get_setting_value('userinfo');
+
+        // Define each element separated.
+        $quizgame = new backup_nested_element('quizgame', array('id'), array(
+            'course', 'name', 'intro', 'introformat', 'timecreated',
+            'timemodified', 'questioncategory', 'grade'));
+
+        $scores = new backup_nested_element('scores');
+
+        $score = new backup_nested_element('score', array('id'), array(
+            'quizgameid', 'userid', 'score'));
+        // Build the tree.
+
+        $quizgame->add_child($scores);
+        $scores->add_child($score);
+
+        // Define sources.
+
+        $quizgame->set_source_table('quizgame', array('id' => backup::VAR_ACTIVITYID));
+
+        // All the rest of elements only happen if we are including user info.
+        if ($userinfo) {
+
+            $score->set_source_sql('
+            SELECT *
+              FROM {quizgame_scores}
+             WHERE quizgameid = ?',
+            array(backup::VAR_PARENTID));
+
+        }
+
+        // Define id annotations.
+        $score->annotate_ids('user', 'userid');
+
+
+        // Define file annotations.
+        $quizgame->annotate_files('mod_quizgame', 'intro', null); // This file area hasn't itemid.
+
+        // Return the root element (quizgame), wrapped into standard activity structure.
+        return $this->prepare_activity_structure($quizgame);
+
+    }
+}
---
+++ backup/moodle2/restore_quizgame_stepslib.php
@@ -0,0 +1,82 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package mod_quizgame
+ * @subpackage backup-moodle2
+ * @copyright 2018 Stephen Bourget
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Structure step to restore one quizgame activity.
+ */
+class restore_quizgame_activity_structure_step extends restore_activity_structure_step {
+
+    protected function define_structure() {
+
+        $paths = array();
+        $userinfo = $this->get_setting_value('userinfo');
+
+        $paths[] = new restore_path_element('quizgame', '/activity/quizgame');
+        if ($userinfo) {
+            $paths[] = new restore_path_element('quizgame_score', '/activity/quizgame/scores/score');
+        }
+
+        // Return the paths wrapped into standard activity structure.
+        return $this->prepare_activity_structure($paths);
+    }
+
+    protected function process_quizgame($data) {
+        global $DB;
+
+        $data = (object)$data;
+        $oldid = $data->id;
+        $data->course = $this->get_courseid();
+
+        $data->userid = $this->get_mappingid('user', $data->userid);
+
+        // Map the category in the QB.
+        $data->questioncategory = $this->get_mappingid('question_category', $data->questioncategory);
+        // Insert the quizgame record.
+        $newitemid = $DB->insert_record('quizgame', $data);
+        // Immediately after inserting "activity" record, call this.
+        $this->apply_activity_instance($newitemid);
+        $this->set_mapping('quizgame', $oldid, $newitemid);
+    }
+
+    protected function process_quizgame_score($data) {
+        global $DB;
+
+        $data = (object)$data;
+        $oldid = $data->id;
+
+        $data->quizgameid = $this->get_new_parentid('quizgame');
+        $data->userid = $this->get_mappingid('user', $data->userid);
+
+        $newitemid = $DB->insert_record('quizgame_scores', $data);
+        $this->set_mapping('quizgame_scores', $oldid, $newitemid, true); // Files by this itemname.
+    }
+
+
+    protected function after_execute() {
+        // Add quizgame related files, no need to match by itemname (just internally handled context).
+        $this->add_related_files('mod_quizgame', 'intro', null);
+
+    }
+}
