af5672483ca2f0904c842dd64b0e949736549ff9
Stephen Bourget <Stephen.Bourget@sau19.org>
2022-01-14 16:28:52 -0500

    mod_quizgame: Bump version and dependancies for Moodle 4.0

--- version.php
+++ version.php
@@ -28,9 +28,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2021042700;      // If version == 0 then module will not be installed.
-$plugin->requires = 2021042300;      // Requires Moodle version 3.11 or later.
+$plugin->version  = 2022011400;      // If version == 0 then module will not be installed.
+$plugin->requires = 2022011400;      // Requires Moodle version 4.0 Alpha or later.
 $plugin->cron      = 0;              // Period for cron to check this module in seconds.
 $plugin->component = 'mod_quizgame'; // To check on upgrade, that module sits in correct place.
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v3.11-r1';
+$plugin->release = 'v4.0-r1';
